535c2e5fdb62536b8c821ba188f8fc8c7cf4219c
DawnTrick <ahmetbilalturan1@gmail.com>
2022-12-24 20:07:39 +0300

    son

--- methods/commentactions.js
+++ methods/commentactions.js
@@ -1,5 +1,6 @@
 var Comment = require('../models/comment')
 var Location = require('../models/location')
+var District = require('../models/district')
 
 var functions={
     
@@ -27,9 +28,16 @@ var functions={
                         else{
                            Comment.aggregate([{$match: {commentLocationId: req.body.locationId}},{$group:{_id: null, average: {$avg: '$commentRating'}}},], function(err, result){
                                 console.log()
-                                Location.findOneAndUpdate({_id: req.body.locationId},{locationAvarageRating: parseFloat((result[0])['average'])}, function(err, location){
+                                Location.findOneAndUpdate({_id: req.body.locationId},{locationAvarageRating: parseFloat((result[0])['average'])}, function(err){
                                     if(err) throw err
-                                    res.json({success: true, msg: 'Başarıyla yorum eklendi'})
+                                    Location.aggregate([{$match: {locationDistrictId: location.locationDistrictId}}, {$group:{_id: null, average:{$avg: '$locationAvarageRating'}}}], function(err,result){
+                                        if(err) throw err
+                                        District.findOneAndUpdate({_id: location.locationDistrictId},{districtAvarageRating: parseFloat((result[0])['average'])}, function(err){
+                                            if(err) throw err
+                                            res.json({success: true, msg: 'Başarıyla yorum eklendi'})
+                                        })
+                                    })
+                                    
                                     
                                 })
                             });
